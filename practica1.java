93ad4bb88128214cf24af69edc18ebfc063e74f1
jocelyn-25 <jocelar24jim201@gmail.com>
2026-03-18 18:16:52 -0600

    Add files via upload

---
+++ practica1.java
@@ -0,0 +1,11 @@
+package src;
+
+public class practical {
+    int numero1 = 5;
+    int numero2 = 10;
+
+    public static void main(String[] args) {
+        practical obj = new practical();
+        System.out.println("Hola Mundo " + (obj.numero1 + obj.numero2));
+    }
+}
